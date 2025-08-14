f3449f0a6d7142debc7c0440727d859a18fff3be
Olena-Olkhovyk <alyonaeremen@gmail.com>
2025-08-14 19:03:22 +0200

    feat: add footer

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+      <div class="wrapper">
+        <p class="text-center">2025 All rights reserved, Tasy Food. Developed by Olena.</p>
+      </div>
+    </div>
+</body>
+</html>
