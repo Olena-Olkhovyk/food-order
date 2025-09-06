9eb479645ead50db9471f5136aa972d4dff4eb68
Olena-Olkhovyk <alyonaeremen@gmail.com>
2025-09-06 22:06:11 +0200

    add files for managing admin, category, food, order

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -2,7 +2,45 @@
     <div class="main-content">
       <div class="wrapper">
        <h1>Manage Admin</h1>
-       <div class="clearfix"></div>
+       <br>
+       <a class="btn-primary" href="add-admin.php">Add Admin</a>
+       <br>
+       <br>
+       <table class="tbl-full">
+        <tr>
+          <th>S.N.</th>
+          <th>Full Name</th>
+          <th>Username</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>1.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+            <a href="#" class="btn-secondary"> Update Admin</a>
+            <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+           <a href="#" class="btn-secondary"> Update Admin</a>
+           <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+        <tr>
+          <td>3.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+           <a href="#" class="btn-secondary"> Update Admin</a>
+           <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+       </table>
       </div>
     </div>
 <?php include("partials/footer.php"); ?>
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -2,6 +2,45 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Category</h1>
+        <br>
+       <a class="btn-primary" href="#">Add Category</a>
+       <br>
+       <br>
+       <table class="tbl-full">
+        <tr>
+          <th>S.N.</th>
+          <th>Full Name</th>
+          <th>Username</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>1.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+            <a href="#" class="btn-secondary"> Update Admin</a>
+            <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+           <a href="#" class="btn-secondary"> Update Admin</a>
+           <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+        <tr>
+          <td>3.</td>
+          <td>Olena</td>
+          <td>Olenka</td>
+          <td>
+           <a href="#" class="btn-secondary"> Update Admin</a>
+           <a href="#" class="btn-danger"> Delete Admin</a>
+          </td>
+        </tr>
+       </table>
     </div>
 </div>
 <?php include("partials/footer.php") ?>
